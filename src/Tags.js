c9542903782d08e81d6004c02a443b0cd836ea6c
Wadim Kalmykov <w@wadim-k.com>
2020-08-01 22:42:09 +0700

    get categories & child tags

---
+++ src/Tags.js
@@ -0,0 +1,39 @@
+import { tags as getAPITags } from './API.js'
+import pkg from 'lodash'
+const { get } = pkg
+
+let cachedAllTags
+
+const allTags = async () => {
+    if (cachedAllTags) return cachedAllTags
+
+    const apiTags = await getAPITags()
+    if (!apiTags) throw new Error('API Tags are undefined.')
+
+    //flatten the object and filter out hidden tags
+    cachedAllTags = apiTags.data
+        .map((t) => {
+            //each tag has either zero or one parent.
+            const parentId = get(t, 'relationships.parent.data.id')
+            return {
+                id: t.id,
+                ...t.attributes,
+                parentId
+            }
+        })
+        .filter((t) => !t.isHidden)
+
+    return cachedAllTags
+}
+
+export const categoryTags = async () => {
+    const tags = await allTags()
+    return tags.filter((t) => t.type === 'primary' && t.isChild === false)
+}
+
+export const childTags = async (parentId) => {
+    const tags = await allTags()
+    return tags.filter((t) => t.type === 'primary' && t.parentId === parentId)
+}
+
+// childTags('12').then((r) => console.log(r))
